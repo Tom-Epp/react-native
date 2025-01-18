b60c345a1b4b5f29099e967669e10eba549b2f62
Thomas Epp <tomepp.dev@gmail.com>
2025-01-18 17:02:33 +0000

    chore: add SocialMediaLinks

---
+++ rn-mastery/apps/digital-business-card/src/utils/constants.ts
@@ -0,0 +1,6 @@
+export const SocialMediaLinks = {
+  at: 'tomepp.dev@gmail.com',
+  github: 'https://github.com/Tom-Epp',
+  linkedin: ' https://www.linkedin.com/in/thomas-epp-472a19a8/',
+  'x-twitter': 'https://x.com/nottomepp',
+};
