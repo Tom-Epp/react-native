458b722daba42cc28b3421d3008865146d481087
Thomas Epp <tomepp.dev@gmail.com>
2025-01-18 12:42:07 +0000

    feat: use safe area context

--- rn-mastery/apps/digital-business-card/src/app/App.tsx
+++ rn-mastery/apps/digital-business-card/src/app/App.tsx
@@ -8,90 +8,94 @@ import {
   ScrollView,
   Button,
   Linking,
-  SafeAreaView,
 } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { FontAwesome6 as FAIcon } from '@expo/vector-icons';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { DisplayInsets } from './DisplayInsets';
 
 export const App = () => {
   const onContactMe = async () =>
     await Linking.openURL('mailto:tomepp.dev@gmail.com');
 
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <View style={styles.container}>
-          <Image
-            source={{
-              uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/0.jpeg',
-            }}
-            style={{ width: '100%', aspectRatio: 16 / 9 }}
-          />
-          <Image
-            style={{
-              width: 150,
-              height: 150,
-              borderRadius: 150,
-              borderWidth: 5,
-              borderColor: 'white',
-              marginTop: -75,
-            }}
-            source={require('../../assets/tom.jpg')}
-          />
-          <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Thomas Epp</Text>
-          <Text>Senior Software Engineer</Text>
-          <View
-            style={{
-              flex: 1,
-              flexDirection: 'row',
-              gap: 10,
-              marginVertical: 10,
-            }}
-          >
-            {['github', 'x-twitter', 'linkedin', 'at'].map(social => (
-              <FAIcon
-                name={social}
-                size={24}
-                color="black"
-                key={`${social}-icon`}
-              />
-            ))}
+    <SafeAreaProvider>
+      <SafeAreaView edges={['bottom']}>
+        <ScrollView>
+          <View style={styles.container}>
+            <Image
+              source={{
+                uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/0.jpeg',
+              }}
+              style={{ width: '100%', aspectRatio: 16 / 9 }}
+            />
+            <Image
+              style={{
+                width: 150,
+                height: 150,
+                borderRadius: 150,
+                borderWidth: 5,
+                borderColor: 'white',
+                marginTop: -75,
+              }}
+              source={require('../../assets/tom.jpg')}
+            />
+            <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Thomas Epp</Text>
+            <Text>Senior Software Engineer</Text>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: 'row',
+                gap: 10,
+                marginVertical: 10,
+              }}
+            >
+              {['github', 'x-twitter', 'linkedin', 'at'].map(social => (
+                <FAIcon
+                  name={social}
+                  size={24}
+                  color="black"
+                  key={`${social}-icon`}
+                />
+              ))}
+            </View>
+            <DisplayInsets />
+            <Button title="Contact Me" onPress={onContactMe} />
+            <Text
+              style={{
+                padding: 20,
+                fontSize: 16,
+                textAlign: 'justify',
+              }}
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Nam
+              aliquam sem et tortor consequat id porta nibh. Pellentesque nec
+              nam aliquam sem. Placerat duis ultricies lacus sed. Non curabitur
+              gravida arcu ac tortor dignissim convallis aenean. Amet nisl purus
+              in mollis nunc. Vel elit scelerisque mauris pellentesque pulvinar
+              pellentesque. Sagittis orci a scelerisque purus semper eget duis
+              at tellus. Sed libero enim sed faucibus turpis in eu mi bibendum.
+              Duis at consectetur lorem donec massa sapien faucibus et molestie.
+              At ultrices mi tempus imperdiet nulla malesuada pellentesque elit
+              eget. Purus sit amet volutpat consequat mauris nunc congue nisi
+              vitae. Urna condimentum mattis pellentesque id nibh tortor id.
+              Consequat id porta nibh venenatis. Lectus vestibulum mattis
+              ullamcorper velit sed ullamcorper. Mauris a diam maecenas sed enim
+              ut sem. Volutpat commodo sed egestas egestas fringilla phasellus.
+              Turpis egestas integer eget aliquet nibh praesent tristique magna
+              sit. Congue mauris rhoncus aenean vel elit scelerisque. Tellus
+              integer feugiat scelerisque varius morbi enim. Consectetur a erat
+              nam at. Bibendum arcu vitae elementum curabitur vitae nunc. Sit
+              amet consectetur adipiscing elit. Rhoncus mattis rhoncus urna
+              neque viverra justo. Malesuada pellentesque elit eget gravida.
+              Vitae nunc sed velit dignissim sodales ut eu sem integer.
+            </Text>
+            <StatusBar style="light" />
           </View>
-          <Button title="Contact Me" onPress={onContactMe} />
-          <Text
-            style={{
-              padding: 20,
-              fontSize: 16,
-              textAlign: 'justify',
-            }}
-          >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Nam
-            aliquam sem et tortor consequat id porta nibh. Pellentesque nec nam
-            aliquam sem. Placerat duis ultricies lacus sed. Non curabitur
-            gravida arcu ac tortor dignissim convallis aenean. Amet nisl purus
-            in mollis nunc. Vel elit scelerisque mauris pellentesque pulvinar
-            pellentesque. Sagittis orci a scelerisque purus semper eget duis at
-            tellus. Sed libero enim sed faucibus turpis in eu mi bibendum. Duis
-            at consectetur lorem donec massa sapien faucibus et molestie. At
-            ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget.
-            Purus sit amet volutpat consequat mauris nunc congue nisi vitae.
-            Urna condimentum mattis pellentesque id nibh tortor id. Consequat id
-            porta nibh venenatis. Lectus vestibulum mattis ullamcorper velit sed
-            ullamcorper. Mauris a diam maecenas sed enim ut sem. Volutpat
-            commodo sed egestas egestas fringilla phasellus. Turpis egestas
-            integer eget aliquet nibh praesent tristique magna sit. Congue
-            mauris rhoncus aenean vel elit scelerisque. Tellus integer feugiat
-            scelerisque varius morbi enim. Consectetur a erat nam at. Bibendum
-            arcu vitae elementum curabitur vitae nunc. Sit amet consectetur
-            adipiscing elit. Rhoncus mattis rhoncus urna neque viverra justo.
-            Malesuada pellentesque elit eget gravida. Vitae nunc sed velit
-            dignissim sodales ut eu sem integer.
-          </Text>
-          <StatusBar style="auto" />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+        </ScrollView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
 const styles = StyleSheet.create({
