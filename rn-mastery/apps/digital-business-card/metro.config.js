453fd5506068446bbde50eb81975f0fbc3ef0430
Thomas Epp <tomepp.dev@gmail.com>
2025-01-19 19:05:08 +0000

    chore: add config

--- rn-mastery/apps/digital-business-card/metro.config.js
+++ rn-mastery/apps/digital-business-card/metro.config.js
@@ -1,41 +1,43 @@
 const { withNxMetro } = require('@nx/expo');
 const { getDefaultConfig } = require('@expo/metro-config');
 const { mergeConfig } = require('metro-config');
-const { withNativeWind } = require("nativewind/metro");
+const { withNativeWind } = require('nativewind/metro');
 
-async function config(){
+async function config() {
+  const defaultConfig = getDefaultConfig(__dirname);
+  const { assetExts, sourceExts } = defaultConfig.resolver;
 
-const defaultConfig = getDefaultConfig(__dirname);
-const { assetExts, sourceExts } = defaultConfig.resolver;
+  /**
+   * Metro configuration
+   * https://reactnative.dev/docs/metro
+   *
+   * @type {import('metro-config').MetroConfig}
+   */
+  const customConfig = {
+    cacheVersion: 'digital-business-card',
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'cjs', 'mjs', 'svg'],
+    },
+  };
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const customConfig = {
-  cacheVersion: 'digital-business-card',
-  transformer: {
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
-  },
-  resolver: {
-    assetExts: assetExts.filter((ext) => ext !== 'svg'),
-    sourceExts: [...sourceExts, 'cjs', 'mjs', 'svg'],
-  },
-};
+  const nxMetroConfig = await withNxMetro(
+    mergeConfig(defaultConfig, customConfig),
+    {
+      // Change this to true to see debugging info.
+      // Useful if you have issues resolving modules
+      debug: false,
+      // all the file extensions used for imports other than 'ts', 'tsx', 'js', 'jsx', 'json'
+      extensions: [],
+      // Specify folders to watch, in addition to Nx defaults (workspace libraries and node_modules)
+      watchFolders: [],
+    }
+  );
 
-const nxMetroConfig = await withNxMetro(mergeConfig(defaultConfig, customConfig), {
-  // Change this to true to see debugging info.
-  // Useful if you have issues resolving modules
-  debug: false,
-  // all the file extensions used for imports other than 'ts', 'tsx', 'js', 'jsx', 'json'
-  extensions: [],
-  // Specify folders to watch, in addition to Nx defaults (workspace libraries and node_modules)
-  watchFolders: [],
-});
-
-return  withNativeWind(nxMetroConfig, { input: "./global.css" });
+  return withNativeWind(nxMetroConfig, { input: './global.css' });
 }
 
 module.exports = config();
---
+++ rn-mastery/apps/quiz/src/components/QuizScreen.tsx
@@ -0,0 +1,33 @@
+import { View, Text, SafeAreaView, Pressable } from 'react-native';
+import { QuestionCard } from '@components/QuestionCard';
+import { FontAwesome6 as FAIcon } from '@expo/vector-icons';
+import Questions from '@/questions';
+
+export function QuizScreen() {
+  const [question] = Questions;
+  return (
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="flex flex-1 bg-white items-center justify-between">
+        <View>
+          <Text>Question 1/5</Text>
+        </View>
+        <View className="bg-amber-300 w-full items-center gap-2">
+          <QuestionCard question={question} />
+          <Text className="">20 sec</Text>
+        </View>
+        <Pressable
+          className="bg-[#005055] rounded-full w-11/12 p-5 items-center justify-center"
+          onPress={() => console.log('Press!')}
+        >
+          <Text className="text-white text-lg leading-normal">Next</Text>
+          <FAIcon
+            name="arrow-right-long"
+            size={16}
+            color="white"
+            className="absolute right-0 mr-4"
+          />
+        </Pressable>
+      </View>
+    </SafeAreaView>
+  );
+}
