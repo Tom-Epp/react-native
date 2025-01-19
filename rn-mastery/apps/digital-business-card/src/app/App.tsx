73904b3c82b94753c50d0d450f9b20bd9d4d1ed4
Thomas Epp <tomepp.dev@gmail.com>
2025-01-19 13:58:26 +0000

    style: use tailwind

--- rn-mastery/apps/digital-business-card/src/app/App.tsx
+++ rn-mastery/apps/digital-business-card/src/app/App.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import {
   Image,
-  StyleSheet,
   View,
   Text,
   ScrollView,
@@ -15,6 +14,7 @@ import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { DisplayInsets } from '@/components/DisplayInsets';
 import { SocialMediaIcons } from '@components/SocialMediaIcons';
 import { SocialMediaLinks } from '@utils/constants';
+import ProfileImage from '@assets/tom.jpg'
 import "../../global.css";
 
 export const App = () => {
@@ -24,21 +24,14 @@ export const App = () => {
 
   React.useEffect(() => {
     (async () => {
-      await new Promise(resolve => setTimeout(resolve, 3000));
+      await new Promise(resolve => setTimeout(resolve, 5200));
       setLoading(false);
     })();
   }, []);
 
   if (loading) {
     return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          gap: 10,
-        }}
-      >
+      <View className="flex-1 justify-center items-center gap-2.5">
         <ActivityIndicator size="small" />
         <Text>Fetching data..</Text>
       </View>
@@ -49,36 +42,22 @@ export const App = () => {
     <SafeAreaProvider>
       <SafeAreaView edges={['bottom']}>
         <ScrollView>
-          <View style={styles.container}>
+          <View className='flex-1 justify-center items-center'>
             <Image
-              source={{
-                uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/0.jpeg',
-              }}
-              style={{ width: '100%', aspectRatio: 16 / 9 }}
+              className='w-full aspect-video'
+              source={{uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/0.jpeg',}}
             />
             <Image
-              style={{
-                width: 150,
-                height: 150,
-                borderRadius: 150,
-                borderWidth: 5,
-                borderColor: 'white',
-                marginTop: -75,
-              }}
-              source={require('../../assets/tom.jpg')}
+              className="size-36 rounded-full border-4 border-white -mt-20"
+              source={ProfileImage}
             />
-            <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Thomas Epp</Text>
-            <Text>Senior Software Engineer</Text>
+            <Text className="font-bold text-3xl">Thomas Epp</Text>
+            <Text className='font-medium'>Senior Software Engineer</Text>
             <SocialMediaIcons />
+
             <DisplayInsets />
             <Button title="Contact Me" onPress={onContactMe} />
-            <Text
-              style={{
-                padding: 20,
-                fontSize: 16,
-                textAlign: 'justify',
-              }}
-            >
+            <Text className='p-5 text-md text-justify'>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Nam
               aliquam sem et tortor consequat id porta nibh. Pellentesque nec
@@ -109,134 +88,5 @@ export const App = () => {
     </SafeAreaProvider>
   );
 };
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  scrollView: {
-    backgroundColor: '#ffffff',
-  },
-  codeBlock: {
-    backgroundColor: 'rgba(55, 65, 81, 1)',
-    marginVertical: 12,
-    padding: 12,
-    borderRadius: 4,
-  },
-  monospace: {
-    color: '#ffffff',
-    fontFamily: 'Courier New',
-    marginVertical: 4,
-  },
-  comment: {
-    color: '#cccccc',
-  },
-  marginBottomSm: {
-    marginBottom: 6,
-  },
-  marginBottomMd: {
-    marginBottom: 18,
-  },
-  marginBottomLg: {
-    marginBottom: 24,
-  },
-  textLight: {
-    fontWeight: '300',
-  },
-  textBold: {
-    fontWeight: '500',
-  },
-  textCenter: {
-    textAlign: 'center',
-  },
-  text2XS: {
-    fontSize: 12,
-  },
-  textXS: {
-    fontSize: 14,
-  },
-  textSm: {
-    fontSize: 16,
-  },
-  textMd: {
-    fontSize: 18,
-  },
-  textLg: {
-    fontSize: 24,
-  },
-  textXL: {
-    fontSize: 48,
-  },
-  textContainer: {
-    marginVertical: 12,
-  },
-  textSubtle: {
-    color: '#6b7280',
-  },
-  section: {
-    marginVertical: 12,
-    marginHorizontal: 12,
-  },
-  shadowBox: {
-    backgroundColor: 'white',
-    borderRadius: 24,
-    shadowColor: 'black',
-    shadowOpacity: 0.15,
-    shadowOffset: {
-      width: 1,
-      height: 4,
-    },
-    shadowRadius: 12,
-    padding: 24,
-    marginBottom: 24,
-  },
-  listItem: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  listItemTextContainer: {
-    marginLeft: 12,
-    flex: 1,
-  },
-  appTitleText: {
-    paddingTop: 12,
-    fontWeight: '500',
-  },
-  hero: {
-    borderRadius: 12,
-    backgroundColor: '#143055',
-    padding: 36,
-    marginBottom: 24,
-  },
-  heroTitle: {
-    flex: 1,
-    flexDirection: 'row',
-  },
-  heroTitleText: {
-    color: '#ffffff',
-    marginLeft: 12,
-  },
-  heroText: {
-    color: '#ffffff',
-    marginVertical: 12,
-  },
-  whatsNextButton: {
-    backgroundColor: '#ffffff',
-    paddingVertical: 16,
-    borderRadius: 8,
-    width: '50%',
-    marginTop: 24,
-  },
-  learning: {
-    marginVertical: 12,
-  },
-  love: {
-    marginTop: 12,
-    justifyContent: 'center',
-  },
-});
 
 export default App;
