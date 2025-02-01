2ca481be60457e16d7e926246324ce333067ba86
Thomas Epp <tomepp.dev@gmail.com>
2025-02-01 22:40:22 +0000

    chore: setup nativewind for camera app

--- rn-mastery/apps/camera/src/app/index.tsx
+++ rn-mastery/apps/camera/src/app/index.tsx
@@ -1,9 +1,12 @@
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView } from 'react-native';
+import '../../global.css';
 
 export default function Index() {
   return (
-    <View>
-      <Text>Home page</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-red-400">
+      <View className="flex-1 items-center justify-center">
+        <Text>Home page</Text>
+      </View>
+    </SafeAreaView>
   );
 }
